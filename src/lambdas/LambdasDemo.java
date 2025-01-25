8d4e37dc2b5e4492922c993941523f6cb74d90e9
mathiasepc <mathiasepc@msn.com>
2025-01-25 22:10:39 +0100

    Lambdas færdig

---
+++ src/lambdas/LambdasDemo.java
@@ -0,0 +1,108 @@
+package lambdas;
+
+import java.util.List;
+import java.util.function.*;
+
+public class LambdasDemo {
+    public static void show() {
+        BinaryOperator<Integer> add = Integer::sum;
+        Function<Integer,Integer> square = a -> a*a;
+        var result = add.andThen(square).apply(1,2);
+        // System.out.println(result);
+
+        UnaryOperator<Integer> square2 = a -> a*a;
+        var result2 = square2.andThen(square).apply(2);
+        System.out.println(result2);
+
+    }
+
+/* !!! Predicate !!!
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+
+        System.out.println(isLongerThan5.test("hej"));
+        System.out.println(isLongerThan5.test("testee"));
+
+        Predicate<String> leftBrace = str -> str.startsWith("{");
+        Predicate<String> rightBrace = str -> str.endsWith("}");
+        boolean result = leftBrace
+                .and(rightBrace)
+                .test("{Testee}");
+        System.out.println(result);
+
+        // .negate() Er det samme som at skrive not='!'.
+        // return true
+        System.out.println(leftBrace.negate().test("w"));
+* */
+
+/* !!! Interface Function<T,R> && Composing Function !!!
+        Function<String, Integer> map =
+                str -> str.length();
+
+        System.out.println(map.apply("string"));
+
+        Function<String,String> replace =
+                str -> str.replace(":", "=");
+        Function<String, String> addBraces =
+                str -> "{" + str + "}";
+
+        // Format af string.
+        var result = replace
+                .andThen(addBraces)
+                .apply("key:value");
+
+        System.out.println(result);
+* */
+
+/* !!! Supplier functional interface !!!
+        Supplier<Double> getRandom = () -> Math.random();
+        System.out.println(getRandom.get());
+* */
+
+/* !!! Consumer functional interface !!!
+        List<String> list = List.of("a", "b", "c");
+        //List<Integer> list = List.of(1,2,3);
+        Consumer<String> print = System.out::println;
+        Consumer<String> printUpperCase = item ->
+                System.out.println(item.toUpperCase());
+
+        list.forEach(print);
+        // Channing
+        list.forEach(print.andThen(printUpperCase).andThen(print));
+
+
+        // Imperative programming
+        for (var item : list)
+            System.out.println(item);
+        // Declarative programming
+        list.forEach(System.out::println);
+* */
+
+/* !!! Lambda !!!
+    public static String prefix = "-";
+
+    public LambdasDemo(String message){
+
+    }
+
+    public static void print(String message){}
+    public static void show() {
+        // Fordi, at vi har et interface, i parameter,
+        // kan vi bruge lambda. Da metoden er void i interface,
+        // tilader den en sout som er void
+        greet(messsage -> System.out.println(prefix + messsage));
+        greet(System.out::println);
+
+        // det samme
+        greet(message -> print(message));
+        greet(LambdasDemo::print);
+
+        // det samme
+        greet(message -> new LambdasDemo(message));
+        greet(LambdasDemo::new);
+    }
+
+    public static void greet(Printer printer) {
+        printer.print("hello world");
+    }
+* */
+}
